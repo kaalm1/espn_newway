86b1452d38a19a941612871100213ee091cc19f0
kaalm1 <kaalm1@gmail.com>
2017-07-25 10:47:53 -0400

    added

--- app.js
+++ app.js
@@ -3,25 +3,10 @@ store = {teams:[],players:[]}
 
 $('document').ready(function(){
 
-  renderPlayers()
-  deletePlayer()
-  deleteTeam()
-})
-
-function render(html,into){
-
-  $(into).empty()
-  $(into).append(html)
-}
-
-
-function renderPlayers(){
   $('body').on('click', 'table.teams tr', function(){
     playersController.show(this)
   })
-}
 
-function deletePlayer(){
   $('body').on('click','.delete-player', function(){
     let id = this.parentElement.id
     let player = Player.find(parseInt(id))
@@ -30,15 +15,18 @@ function deletePlayer(){
     player.destroy()
     render(playersController.playersHTML(team),'table.players tbody')
   })
-}
 
-
-
-function deleteTeam(){
   $('body').on('click','.delete-team',function(){
     let id = this.parentElement.id.replace("team-","")
     let team = Team.find(parseInt(id))
     team.destroy()
     teamsController.show()
   })
+
+})
+
+function render(html,into){
+
+  $(into).empty()
+  $(into).append(html)
 }
